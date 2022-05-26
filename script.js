01c2aaeb3a5cd250a3cff1896021bc0451d063d6
Mary Cancellieri <mary.cancellieri3@gmail.com>
2022-05-25 22:02:49 -0400

    created timer, timer display, and transition into the end Screen

--- script.js
+++ script.js
@@ -4,9 +4,10 @@ var endScreen = document.querySelector("#ending");
 var scoreScreen = document.querySelector("#scoreboard");
 var startBtn = document.querySelector("#start");
 var scoreBtn = document.querySelector(".to-scoreboard");
+var timerEl = document.querySelector("#timer");
 
 // Instructor Provided Code: Anthony Cooper
-var state= "start"
+var state = "start"
 
 function displayState() {
     if (state === "start") {
@@ -30,11 +31,29 @@ function displayState() {
     displayState();
   }
 
+// My contributions continue again here.
   startBtn.addEventListener("click", function (event) {
     event.preventDefault();
     state = "quiz";
-    
+    displayState();
+    setTime();
   });
+
+  var timeLeft = 60;
+
+  function setTime(){
+      var timeInterval = setInterval(function () {
+        timeLeft--;
+        timerEl.textContent = "Time Left: " + timeLeft;
+        // Note to self: Timer currently displays as 59 instead of 60
+
+        if (timeLeft === 0) {
+            clearInterval(timeInterval);
+            state = "end";
+            displayState();
+        }
+      }, 1000)
+  }
 
 
 
