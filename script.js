46bc79593746e2cfb1630f1cdf58640d7722e9bb
Mary Cancellieri <mary.cancellieri3@gmail.com>
2022-05-25 21:16:31 -0400

    Added scoreboard section to HTML; set variables queryselecting ids and classes in JS

---
+++ script.js
@@ -0,0 +1,6 @@
+var startScreen = document.querySelector("#beginning");
+var quizScreen = document.querySelector("#quiz");
+var endScreen = document.querySelector("#ending");
+var scoreScreen = document.querySelector("#scoreboard");
+var startBtn = document.querySelector("#start");
+var scoreBtn = document.querySelector(".to-scoreboard");
