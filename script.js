ea4c131e5f80eb9c6bd479b3a18038452f305cbf
Mary Cancellieri <mary.cancellieri3@gmail.com>
2022-05-25 21:19:01 -0400

    Added Instructor Provided code for function displayState and function init

--- script.js
+++ script.js
@@ -4,3 +4,32 @@ var endScreen = document.querySelector("#ending");
 var scoreScreen = document.querySelector("#scoreboard");
 var startBtn = document.querySelector("#start");
 var scoreBtn = document.querySelector(".to-scoreboard");
+
+// Instructor Provided Code: Anthony Cooper
+var state= "start"
+
+function displayState() {
+    if (state === "start") {
+      startScreen.style.display = "block";
+      quizScreen.style.display = "none";
+      endScreen.style.display = "none";
+    }
+    if (state === "quiz") {
+      startScreen.style.display = "none";
+      quizScreen.style.display = "block";
+      endScreen.style.display = "none";
+    }
+    if (state === "end") {
+      startScreen.style.display = "none";
+      quizScreen.style.display = "none";
+      endScreen.style.display = "block";
+    }
+  }
+
+  function init() {
+    displayState();
+  }
+
+
+
+  init();
