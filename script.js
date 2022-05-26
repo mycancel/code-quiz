1cfdc59e6e0c9871fef18fb288d5c4cedc9bd8b3
Mary Cancellieri <mary.cancellieri3@gmail.com>
2022-05-25 23:47:45 -0400

    added scoreboard section and js display state; set up scoreBtn event listener, data storage, and change in state

--- script.js
+++ script.js
@@ -3,46 +3,60 @@ var quizScreen = document.querySelector("#quiz");
 var endScreen = document.querySelector("#ending");
 var scoreScreen = document.querySelector("#scoreboard");
 var startBtn = document.querySelector("#start");
-var scoreBtn = document.querySelector(".to-scoreboard");
+var scoreBtn = document.querySelector("#to-scoreboard");
+
 var timerEl = document.querySelector("#timer");
+var initials = document.querySelector("#initials");
+
+// Local memory of one highscore
+var pastScores = localStorage.getItem("highscores") || "";
 
 // Instructor Provided Code: Anthony Cooper
 var state = "start"
 
 function displayState() {
     if (state === "start") {
-      startScreen.style.display = "block";
-      quizScreen.style.display = "none";
-      endScreen.style.display = "none";
+        startScreen.style.display = "block";
+        quizScreen.style.display = "none";
+        endScreen.style.display = "none";
+        scoreScreen.style.display = "none";
     }
     if (state === "quiz") {
-      startScreen.style.display = "none";
-      quizScreen.style.display = "block";
-      endScreen.style.display = "none";
+        startScreen.style.display = "none";
+        quizScreen.style.display = "block";
+        endScreen.style.display = "none";
+        scoreScreen.style.display = "none";
     }
     if (state === "end") {
-      startScreen.style.display = "none";
-      quizScreen.style.display = "none";
-      endScreen.style.display = "block";
+        startScreen.style.display = "none";
+        quizScreen.style.display = "none";
+        endScreen.style.display = "block";
+        scoreScreen.style.display = "none";
     }
-  }
+    if (state === "score") {
+        startScreen.style.display = "none";
+        quizScreen.style.display = "none";
+        endScreen.style.display = "none";
+        scoreScreen.style.display = "block";
+    }
+}
 
-  function init() {
+function init() {
     displayState();
-  }
+}
 
-// My contributions continue again here.
-  startBtn.addEventListener("click", function (event) {
+startBtn.addEventListener("click", function (event) {
     event.preventDefault();
     state = "quiz";
     displayState();
     setTime();
-  });
+});
 
-  var timeLeft = 60;
+var timeLeft = 5;
+//   Note to self: Remember to set time back to 60
 
-  function setTime(){
-      var timeInterval = setInterval(function () {
+function setTime() {
+    var timeInterval = setInterval(function () {
         timeLeft--;
         timerEl.textContent = "Time Left: " + timeLeft;
         // Note to self: Timer currently displays as 59 instead of 60
@@ -52,9 +66,16 @@ function displayState() {
             state = "end";
             displayState();
         }
-      }, 1000)
-  }
+    }, 1000)
+}
+
+scoreBtn.addEventListener("click", function (event) {
+    event.preventDefault();
+    var highscore = initials.value.trim() + " — " + timeLeft;
+    localStorage.setItem("highscores", highscore);
+    state = "score";
+    displayState();
+});
 
 
-
-  init();
+init();
